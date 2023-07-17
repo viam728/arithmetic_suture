f9f72689225f5e6d9f00ea63e886cba6473099d5
viam728 <guge1657203672@gmail.com>
2023-07-18 02:03:45 +0800

    laptop

--- arithmetic_c/hello.cpp
+++ arithmetic_c/hello.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 using namespace std;
-
 int main (){
     printf("Hello@");
     cout<<endl;
-    string a="viam728";
-    cin>> a ;
-    cout<< a <<endl; 
+    string a = "viam728";
+    // cin>> a ;
+    int b = 0;
+    double d=1;
+    cout<< a <<endl;
+    scanf("%d",&b);
+    printf("%d",b);
+    printf("%f", d);
+    
 }
