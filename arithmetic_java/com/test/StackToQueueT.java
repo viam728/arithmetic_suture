ea3390a26a0d12e2566db0772f11172d93c41260
viam728 <guge1657203672@gmail.com>
2023-07-17 22:52:03 +0800

    new

---
+++ arithmetic_java/com/test/StackToQueueT.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class StackToQueueT {
+    public static void main(String[] args) {
+        QueueByStack queueByStack = new QueueByStack();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int val = 0;
+            int inp = scanner.nextInt();
+            while (inp>0){
+                val = scanner.nextInt();
+                queueByStack.add(val);
+                inp --;
+            }
+            System.out.println(String.valueOf(queueByStack.peek()));
+            System.out.println(String.valueOf(queueByStack.poll()));
+            System.out.println(String.valueOf(queueByStack.peek()));
+
+        }
+    }
+}
